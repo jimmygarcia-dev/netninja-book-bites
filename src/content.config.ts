e309a7d24d9a63d6eaa5f24ca6d363afaa8f9a77
Jimmy García <jimmyegc@gmail.com>
2026-04-09 16:07:46 -0600

    04092026

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,10 @@
-import { file } from "astro/loaders"
+import { glob } from "astro/loaders"
 import { z } from "astro/zod"
 import { defineCollection } from "astro:content"
 
 const books = defineCollection({
-  loader: file('src/content/books.json'),
-  schema: z.object({
-    id: z.string(),
+  loader: glob({ base: 'src/content/books', pattern: '*.md'}),
+  schema: z.object({    
     title: z.string(),
     author: z.string(),
     rating: z.number(),
