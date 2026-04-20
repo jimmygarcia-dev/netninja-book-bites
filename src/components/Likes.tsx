f75b282c0450109896cfe0d1db445c470e596e40
Jimmy García <jimmyegc@gmail.com>
2026-04-20 09:59:39 -0600

    04202026

---
+++ src/components/Likes.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const Likes = () => {
+  const [likes, setLikes] = useState(3);
+
+  const handleClick = () => {
+    setLikes((prev) => prev + 1);
+  };
+
+  return (
+    <div className="likes">
+      <p>{likes} people like this review.</p>
+      <button onClick={handleClick}>Like</button>
+    </div>
+  );
+};
+
+
+export default Likes
